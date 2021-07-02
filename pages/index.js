9fc29b2edd4c57bae7d71ac0ed812a0beaa7ff58
shuvomohajan <shuvomohajan@gmail.com>
2021-07-02 19:04:13 +0600

    small design change and new project added

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       </Head>
 
       <div className={styles.bg_design}></div>
+      <div className={styles.bg_design2}></div>
 
       <main className="min-h-screen py-14 text-center">
         <p>Hey There!</p>
@@ -31,26 +32,42 @@ export default function Home() {
         <div className="grid grid-cols-2 md:grid-cols-3 gap-2.5 md:gap-4 mt-10">
           <a href="https://pixelSensePark.com/" target="_blank" className={styles.card}>
             <h3>PixelSensePark.com</h3>
+            <p>Protfolio & Proto Editing Solution</p>
           </a>
 
           <a href="https://aonmart.net/" target="_blank" className={styles.card}>
             <h3>AonMart.net</h3>
-          </a>
-
-          <a href="https://boionlinebd.com/" target="_blank" className={styles.card}>
-            <h3>BoiOnlineBD.com</h3>
+            <p>Wirehouse and Multi Store Inventory with Mobile App E-commerce</p>
           </a>
 
           <a href="https://esomoybangla.com/" target="_blank" className={styles.card}>
             <h3>EsomoyBangla.com</h3>
+            <p>Online News Portal</p>
+          </a>
+
+          <a href="https://rabbiitfirm.com/" target="_blank" className={styles.card}>
+            <h3>RabbiITFirm.com</h3>
+            <p>Protfolio and SEO Solution</p>
+          </a>
+
+          <a href="https://boionlinebd.com/" target="_blank" className={styles.card}>
+            <h3>BoiOnlineBD.com</h3>
+            <p>Book E-Commerce</p>
           </a>
 
           <a href="https://glolotto.org/" target="_blank" className={styles.card}>
             <h3>GloLotto.org</h3>
+            <p>Thai Lottery Game</p>
           </a>
 
           <a href="https://apnarwallet.com/exchanges.php" target="_blank" className={styles.card}>
             <h3>ApnarWallet.com</h3>
+            <p>Money Exchange</p>
+          </a>
+
+          <a href="https://apnarwallet.com/exchanges.php" target="_blank" className={styles.card}>
+            <h3>Swaty Books</h3>
+            <p>Audio Book Mobile App</p>
           </a>
         </div>
       </main>
