181e7972ee7a22c246217bc623310952ddb67a89
shuvomohajan <shuvomohajan@gmail.com>
2021-07-23 18:43:32 +0600

    trying to fix nextjs image loader

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,6 +1,10 @@
-import Image from 'next/image'
+import Image from "next/image";
 import styles from "./About.module.css";
 import Social from "./Social/Social";
+
+const myLoader = ({ src, width, quality }) => {
+  return `https://shuvomohajan.github.io${src}?w=${width}&q=${quality || 85}`;
+};
 
 const About = () => {
   return (
@@ -11,7 +15,15 @@ const About = () => {
       ].join(" ")}
     >
       <div className="container px-2 md:px-4 mx-auto h-full">
-        <Image src="/shuvo.jpg" height="150" width="150" className="object-cover rounded-full" />
+        <Image
+          loader={myLoader}
+          src="/shuvo.jpg"
+          height="150"
+          width="150"
+          className="object-cover rounded-full"
+          loading="eager"
+          priority="true"
+        />
         <h1 className={styles.title}>
           This is{" "}
           <a href="https://shuvomohajan.github.io/" className="text-white">
@@ -28,7 +40,12 @@ const About = () => {
 
         <Social />
 
-        <a href="#" className="mt-10 py-2.5 px-8 rounded-full font-bold bg-red-500 inline-block cursor-pointer">Hire Me</a>
+        <a
+          href="#"
+          className="mt-10 py-2.5 px-8 rounded-full font-bold bg-red-500 inline-block cursor-pointer"
+        >
+          Hire Me
+        </a>
       </div>
     </div>
   );
