3fa003b8c052ef7d5f708d8d65f0d18671ac4cef
shuvomohajan <34658843+shuvomohajan@users.noreply.github.com>
2021-07-23 16:59:31 +0000

    Deploying to gh-pages from @ shuvomohajan/shuvomohajan.github.io@37f3ef35fcffa4eea99072954f06ef641c37ecc1 🚀

---
+++ _next/static/NFZ4oPCGIbhqihoFnE-V_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b928fa5a284ad3b9f2c1.css","static/chunks/pages/index-1ee283165bbe7e65724a.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ZDcSFCZIGQp3xLrYquZD4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/719f2883b78e142e3df3.css","static/chunks/pages/index-15b0f743b0dae13ad176.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
